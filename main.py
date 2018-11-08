06eba81d4e40221817fb1f40fe7b04b785f6152d
Aaron Duggan <dugganaa@tcd.ie>
2018-11-08 18:31:40 +0000

    +Code to list information about a specified user

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from github import Github
 
-g = Github("dugganaa","not_Actually_my_password")
-for repo in g.get_user().get_repos():
-    print(repo.name)
+username = input('username --> ')
+
+g = Github()
+user = g.get_user(username)
+print(user.name)
+print(user.bio)
+print("Number of collaborators: " + str(user.collaborators))
+print("Company name: " + str(user.company))
+print("Number of contributions: " + str(user.contributions))
+print("Email: " + str(user.email))
+print("Followers: " + str(user.followers))
+print("Following: " + str(user.following))
+print("Hireable: " + str(user.hireable))
+print("ID: " + str(user.id))
+print("Public repos: " + str(user.public_repos))
+print("Private repos: " + str(user.total_private_repos))
+
+
 
 

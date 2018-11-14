08892f216947b5a30c447847f0900de74d176914
Aaron Duggan <dugganaa@tcd.ie>
2018-11-14 20:29:43 +0000

    +code to recursively store no. of followers and no. of contributors in a dataframe

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from github import Github
+import pandas as pd
+
+MAX_ROWS = 200;
 
 username = input('username --> ')
+password = input('password --> ')
 
-g = Github()
-user = g.get_user(username)
+g = Github(str(username), str(password))
+contFollDf = pd.dataframe(data = contFollData, columns=['Contributors', 'Followers'])
+
+user = g.get_user()
+
+
+'''
 print(user.name)
-print(user.bio)
+print("Bio: " + str(user.bio))
 print("Number of collaborators: " + str(user.collaborators))
 print("Company name: " + str(user.company))
 print("Number of contributions: " + str(user.contributions))
@@ -16,4 +25,32 @@ print("Hireable: " + str(user.hireable))
 print("ID: " + str(user.id))
 print("Public repos: " + str(user.public_repos))
 print("Private repos: " + str(user.total_private_repos))
+'''
+followers = user.get_followers()
+print(followers[0].login)
+columns = 2
+
+contFoll(user.login)
+
+def contFoll(username):
+    currentUser = Github(username)
+    
+    if (contFollDf.size() >= MAX_ROWS*columns):
+        return
+    
+    totalFollowers = currentUser.followers
+    totalContributions = currentUser.contributions
+    
+    contFollDf = contFollDf.append({'Contributors' : totalContributors}, {'Followers' : totalFollowers}, ignore_index=True)
+    named_followers = currentUser.get_followers()
+    if (named_followers == None):
+        return
+    
+    i = 0
+    while (contFollDf.size() <= MAX_ROWS * columns and i < named_followers.size()):
+        nextFollower = str(currentUser.named_Followers[i].login)
+        contFoll(nextFollower)
+        i = i + 1
+    
+    
 

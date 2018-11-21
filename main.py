00e137cccbb2197fda8752a2d32950c968d02014
Aaron Duggan <dugganaa@tcd.ie>
2018-11-21 22:55:46 +0000

    Added progress bar

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from github import Github
 import pandas
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.preprocessing import Imputer
 
 MAX_ROWS = 25
 REPO_FOLL_COLUMNS = 2
@@ -12,17 +13,28 @@ user = g.get_user()
 followers = user.get_followers()
 
 df = pandas.DataFrame(columns=['Repos', 'Followers'])
+currentPerc = 0
 
 def repoFoll(username, prev):
     currentUser = g.get_user(username)
     prev.append(currentUser.login)
+
     global df
     df = df.append({'Repos' : currentUser.public_repos, 'Followers' : currentUser.followers}, ignore_index = True)
+
     if (df.size >= MAX_ROWS * REPO_FOLL_COLUMNS):
         return True
+
     named_followers = currentUser.get_followers()
     if named_followers is None:
         return False
+
+    global currentPerc
+    if ((100 * df.size)/(MAX_ROWS * REPO_FOLL_COLUMNS)>=(currentPerc + 10)):
+        currentPerc = currentPerc + 10
+        print('{}% complete'.format(currentPerc))
+
+
     for i in named_followers:
         if (df.size >= MAX_ROWS * REPO_FOLL_COLUMNS):
             return True
@@ -35,5 +47,6 @@ def repoFoll(username, prev):
 print("Interrogating API, this may take a few seconds...")
 repoFoll(username = user.login, prev = [])
 print(df)
+
 df.plot(x='Repos', y='Followers', style= 'o')
 plt.show()

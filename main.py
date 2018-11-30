86a2c91561a5111492c1c66eb5c15f6ec2427dbb
dugganaa <dugganaa@tcd.ie>
2018-11-30 14:33:19 +0000

    Add user input handling

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from github import Github
+import sys
 import pandas
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 
-MAX_ROWS = 40
+DEFAULT_MAX_ROWS = 20
+max_rows = 10
 REPO_FOLL_COLUMNS = 2
 FOLL_NODE_LISTS = 2
 
 class repoFollowers:
 
-    def __init__(self, username, g):
+    def __init__(self, username, g, rows):
         self.username = username
         self.g = g
         self.prev = []
         self.df = pandas.DataFrame(columns=['Repos', 'Followers'])
         self.currentPerc = 0
+        self.rows = rows
 
     def returnDf(self):
         return self.df
@@ -24,19 +27,19 @@ class repoFollowers:
         currentUser = self.g.get_user(self.username)
         self.prev.append(currentUser.login)
         self.df = self.df.append({'Repos' : currentUser.public_repos, 'Followers' : currentUser.followers}, ignore_index = True)
-        if (self.df.size >= MAX_ROWS * REPO_FOLL_COLUMNS):
+        if (self.df.size >= self.rows * REPO_FOLL_COLUMNS):
             return True
 
         named_followers = currentUser.get_followers()
         if named_followers is None:
             return False
 
-        if ((100 * self.df.size)/(MAX_ROWS * REPO_FOLL_COLUMNS)>=(self.currentPerc + 10)):
+        if ((100 * self.df.size)/(self.rows * REPO_FOLL_COLUMNS)>=(self.currentPerc + 10)):
             self.currentPerc = self.currentPerc + 10
             print('{}% complete'.format(self.currentPerc))
 
         for i in named_followers:
-            if (self.df.size >= MAX_ROWS * REPO_FOLL_COLUMNS):
+            if (self.df.size >= self.rows * REPO_FOLL_COLUMNS):
                 return True
             if i.login not in self.prev:
                 nextFollower = str(i.login)
@@ -47,13 +50,14 @@ class repoFollowers:
 
 
 class followersNetwork():
-    def __init__(self, username, g):
+    def __init__(self, username, g, rows):
         self.username = username
         self.g = g
         self.prev = []
         self.a = []
         self.b = []
         self.currentPerc = 0
+        self.rows = rows
 
     def returnG(self):
         self.df = pandas.DataFrame({ 'from':self.a, 'to':self.b})
@@ -63,27 +67,28 @@ class followersNetwork():
     def inter(self):
         currentUser = self.g.get_user(self.username)
         self.prev.append(currentUser.login)
-        if (len(self.a) >= MAX_ROWS * FOLL_NODE_LISTS):
+        if (len(self.a) >= self.rows * FOLL_NODE_LISTS):
             return True
 
         named_followers = currentUser.get_followers()
         if named_followers is None:
             return False
 
-        if ((100 * (len(self.a)))/(MAX_ROWS)>=(self.currentPerc + 10)):
-            self.currentPerc = self.currentPerc + 10
-            print('{}% complete'.format(self.currentPerc))
+
 
         for i in named_followers:
-            if (len(self.a) >= MAX_ROWS):
+            if (len(self.a) >= self.rows):
                 return True
+            if ((100 * (len(self.a)))/(self.rows)>=(self.currentPerc + 10)):
+                self.currentPerc = self.currentPerc + 10
+                print('{}% complete'.format(self.currentPerc))
             if i.login not in self.prev:
                 nextFollower = str(i.login)
                 self.a.append(currentUser.login)
                 self.b.append(nextFollower)
 
         for i in named_followers:
-            if (len(self.a) >= MAX_ROWS):
+            if (len(self.a) >= self.rows):
                 return True
             if i.login not in self.prev:
                 nextFollower = str(i.login)
@@ -93,21 +98,45 @@ class followersNetwork():
         return False
 
 
+successfulLogin = False
 
-token = input('access token --> ')
-g = Github(str(token))
-user = g.get_user()
+for attempts in range(10):
+    try:
+        token = input('Input your access token to begin--> ')
+        g = Github(str(token))
+        user = g.get_user()
+        print("Hello {}, use the following commands to interrogate".format(user.name),
+            "the Github API and display the data.")
+    except:
+        print("Error: Bad Credentials. You inputted '{}'".format(str(token)))
+    else:
+        successfulLogin = True
+        break
 
-plot1 = repoFollowers(user.login,g)
-plot1.inter()
-dataframe1 = plot1.returnDf()
-dataframe1.plot(x='Repos', y='Followers', style='o')
-#plot2 = followersNetwork(user.login,g)
-#plot2.inter()
-#graph2 = plot2.returnG()
-#nx.draw(graph2, with_labels=True)
+if successfulLogin:
+    for x in sys.stdin:
+        args = x.split(" ")
 
+        if len(args) == 1:
+            args.append(DEFAULT_MAX_ROWS)
 
+        if len(args) == 0:
+            print("You must input something!")
 
-
-plt.show()
+        if args[0] == "quit\n" or args[0].lower() == "exit\n":
+            print("Stopping program.")
+            break
+        elif args[0] == "fr":
+            p = repoFollowers(user.login,g,int(args[1]))
+            p.inter()
+            df = p.returnDf()
+            df.plot(x='Repos', y='Followers', style='o')
+            plt.show()
+        elif args[0] == 'fg':
+            p = followersNetwork(user.login,g, int(args[1]))
+            p.inter()
+            g = p.returnG()
+            nx.draw(g, with_labels=True)
+            plt.show()
+        else:
+             print("Error: command not found.")
